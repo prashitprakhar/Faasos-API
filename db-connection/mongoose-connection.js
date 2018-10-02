62fe6cde4392b9d6b65ae2f284f3cccb4592c195
Prashit Prakhar <prashitprakhar@yahoo.com>
2018-10-03 04:38:30 +0530

    deployment

--- db-connection/mongoose-connection.js
+++ db-connection/mongoose-connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://prashitprakhar:faasosdb123!@ds121413.mlab.com:21413/faasosdb' || 'mongodb://localhost:27017/FaasosDB');
+mongoose.connect('mongodb://faasos123:faasos123@ds121753.mlab.com:21753/heroku_jt6ng3gz' || 'mongodb://localhost:27017/FaasosDB');
 
 module.exports = { mongoose };
